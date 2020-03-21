415d9a979e1e88b79e0d0f6b47c89d52ec4d95f8
fedor <fedor993@gmail.com>
2020-03-21 16:34:37 +0300

    added context

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from 'routes';
 import TopBar from 'components/topBar';
+import { CurrentUserProvider } from 'context/currentUser'
 
 const App = () => {
-    return (
-        <div>
-            <Router>
-                <TopBar />
-                <Routes />
-            </Router>
-        </div>
-    )
+   return (
+      <CurrentUserProvider>
+         <Router>
+            <TopBar />
+            <Routes />
+         </Router>
+      </CurrentUserProvider>
+   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
